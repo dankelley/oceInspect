5b2780fa4a8d02780d5d415e5b2720f046980062
dankelley <kelley.dan@gmail.com>
2021-08-19 09:06:34 -0300

    more info at top, better csv name

--- R/oceInspect.R
+++ R/oceInspect.R
@@ -14,14 +14,16 @@ msg <- function(..., eos="\n")
 
 plotHeight <- 500
 pointsize <- 12
+col <- 1
 cex <- 1                               # FIXME: let user select via a UI tool
 pch <- 20                              # FIXME: let user select via a UI tool
 mgp <- c(2, 0.7, 0)
-keyPressHelp <- "FIXME (these are wrong) <ul>
+keyPressHelp <- "Keystroke commands
+<ul>
 <li> 0: save mouse location as category '0' (and similar for 1 through 9)</li>
-<li> u: undo last saved operation</li>
-<li> s: show saved points</li>
-<li> w: write saved points to oceInspect.csv</li>
+<li> u: undo last save</li>
+<li> s: show saved data</li>
+<li> w: write saved data to a csv file</li>
 <li> ?: display this message</li>
 </ul>"
 
@@ -39,7 +41,11 @@ ui <- shiny::fluidPage(
     shiny::headerPanel(title="", windowTitle="oceInspect"),
     shiny::tags$script('$(document).on("keypress", function (e) { Shiny.onInputChange("keypress", e.which); Shiny.onInputChange("keypressTrigger", Math.random()); });'),
     style="text-indent:1em ; background:#e6f3ff ; .btn.disabled { background-color: red; }",
-    shiny::fluidRow(shiny::uiOutput(outputId="UIinfo")),
+    shiny::fluidRow(
+        shiny::column(7,
+            shiny::uiOutput(outputId="UIinfo1")),
+        shiny::column(5,
+            shiny::uiOutput(outputId="UIinfo2"))),
     shiny::fluidRow(
         shiny::column(3,
             shiny::selectInput("plotType", "Plot Type",
@@ -59,11 +65,10 @@ ui <- shiny::fluidPage(
 server <- function(input, output, session)
 {
     data <- NULL
-    lastPoint <- list(x=NULL, y=NULL, i=NULL, view=NULL)
-    allPoints <- list(key=NULL, x=NULL, y=NULL, i=NULL, view=NULL)
+    lastPoint <- list(filename=NULL, x=NULL, y=NULL, i=NULL, view=NULL)
+    allPoints <- list(filename=NULL, key=NULL, x=NULL, y=NULL, i=NULL, view=NULL)
     state <- shiny::reactiveValues(
-        flagged=NULL,
-        zoomed=NULL)
+        savedNumber=0)                 # making this reactive means info2 updates when points are saved
     tmp <- shiny::getShinyOption("data")
     if (!is.null(tmp)) {
         data <- tmp
@@ -73,54 +78,56 @@ server <- function(input, output, session)
             else if (n %in% c("longitude", "latitude", "time"))
                 data@data[[n]] <- data@data[[n]][1]
         }
-        state$flagged <- rep(FALSE, length(data[["pressure"]]))
-        state$zoomed <- rep(TRUE, length(data[["pressure"]]))
     } else {
         stop("must supply a value for the 'data' parameter")
     }
 
-    output$UIinfo <- shiny::renderUI({
-        shiny::verbatimTextOutput("info")
+    output$UIinfo1 <- shiny::renderUI({
+        shiny::verbatimTextOutput("info1")
     })
 
-    output$info <- shiny::renderText({
-        msg <- "Place mouse inside plot to see info."
+    output$UIinfo2 <- shiny::renderUI({
+        shiny::verbatimTextOutput("info2")
+    })
+
+    output$info1 <- shiny::renderText({
+        rval <- "Click in window to activate hover inspection"
         if (!is.null(input$hover$x)) {
-            msg <- sprintf("x=%.4g y=%.4g", input$hover$x, input$hover$y)
             usr <- par("usr") # for scaling (to get closest data point)
             if (input$plotChoice == "TS") {
                 X <- data[["SA"]]
                 Y <- data[["CT"]]
                 p <- data[["pressure"]]
                 dX <- (input$hover$x - X) / (usr[2]-usr[1])
-                dY <- (input$hover$y - Y) / (usr[4]-usr[2])
+                dY <- (input$hover$y - Y) / (usr[4]-usr[3])
                 d <- dX^2 + dY^2
                 i <- which.min(d)
-                msg <- paste0(msg, sprintf(" near %d-th point at %.1f dbar: SA=%.4f, CT=%.4f",
-                        i, p[i], X[i], Y[i]))
             } else if (input$plotChoice == "densitySpice") {
                 X <- data[["spice"]]
                 Y <- data[["sigma0"]]
                 p <- data[["pressure"]]
                 dX <- (input$hover$x - X) / (usr[2]-usr[1])
-                dY <- (input$hover$y - Y) / (usr[4]-usr[2])
+                dY <- (input$hover$y - Y) / (usr[4]-usr[3])
                 d <- dX^2 + dY^2
                 i <- which.min(d)
-                msg <- paste0(msg, sprintf(" near %d-th point at %.1f dbar: spice=%.4f, sigma0=%.4f",
-                        i, p[i], X[i], Y[i]))
             }
+            rval <- sprintf("x=%.4g y=%.4g near %d-th point at %.1f dbar",
+                input$hover$x, input$hover$y, i, p[i])
             lastPoint$view <<- input$plotChoice
             lastPoint$x <<- input$hover$x
             lastPoint$y <<- input$hover$y
             lastPoint$i <<- i
-            #message(str(lastPoint))
         }
-        msg
+        rval
+    })
+
+    output$info2 <- shiny::renderText({
+        csv <- paste0(gsub("\\..*$", "", gsub(".*/","", data[["filename"]])), ".csv")
+        sprintf("%d points in %s ", state$savedNumber, csv)
     })
 
     output$plot <- shiny::renderPlot({
         if (!is.null(data)) {
-            msg(input$plotChoice)
             par(mgp=mgp)
             if (input$plotChoice == "TS") {
                 par(mar=c(3.5,3.5,1.5,1.5))
@@ -153,19 +160,12 @@ server <- function(input, output, session)
             p <- data[["pressure"]]
             lon <- data[["longitude"]][1]
             lat <- data[["latitude"]][1]
-            flagged <- state$flagged
-            if (!is.null(state$zoomed)) {
-                S <- S[state$zoomed]
-                T <- T[state$zoomed]
-                p <- p[state$zoomed]
-                flagged <- flagged[state$zoomed]
-            }
             oce::plotProfile(oce::as.ctd(S, T, p, longitude=lon, latitude=lat),
                 xtype="sigmaTheta",
                 type=maybeNull(input$plotType, "o"),
                 pch=pch,
                 cex=cex,
-                col=if (is.null(flagged)) 1 else ifelse(flagged, 2, 1))
+                col=col)
         }
     }, height=plotHeight, pointsize=pointsize)
 
@@ -177,19 +177,12 @@ server <- function(input, output, session)
             p <- data[["pressure"]]
             lon <- data[["longitude"]][1]
             lat <- data[["latitude"]][1]
-            flagged <- state$flagged
-            if (!is.null(state$zoomed)) {
-                S <- S[state$zoomed]
-                T <- T[state$zoomed]
-                p <- p[state$zoomed]
-                flagged <- flagged[state$zoomed]
-            }
             oce::plotProfile(oce::as.ctd(S, T, p, longitude=lon, latitude=lat),
                 xtype="temperature",
                 type=maybeNull(input$plotType, "o"),
                 pch=pch,
                 cex=cex,
-                col=if (is.null(flagged)) 1 else ifelse(flagged, 2, 1))
+                col=col)
         }
     }, height=plotHeight, pointsize=pointsize)
 
@@ -201,19 +194,12 @@ server <- function(input, output, session)
             p <- data[["pressure"]]
             lon <- data[["longitude"]][1]
             lat <- data[["latitude"]][1]
-            flagged <- state$flagged
-            if (!is.null(state$zoomed)) {
-                S <- S[state$zoomed]
-                T <- T[state$zoomed]
-                p <- p[state$zoomed]
-                flagged <- flagged[state$zoomed]
-            }
             oce::plotProfile(oce::as.ctd(S, T, p, longitude=lon, latitude=lat),
                 xtype="salinity",
                 type=maybeNull(input$plotType, "o"),
                 pch=pch,
                 cex=cex,
-                col=if (is.null(flagged)) 1 else ifelse(flagged, 2, 1))
+                col=col)
         }
     }, height=plotHeight, pointsize=pointsize)
 
@@ -226,31 +212,44 @@ server <- function(input, output, session)
             message(sprintf("%d %f %f %d", as.integer(key), lastPoint$x, lastPoint$y, lastPoint$i))
             n <- length(allPoints$key)
             allPoints$key[n+1] <<- as.integer(key)
+            allPoints$filename[n+1] <<- data[["filename"]]
             allPoints$view[n+1] <<- lastPoint$view
             allPoints$x[n+1] <<- lastPoint$x
             allPoints$y[n+1] <<- lastPoint$y
             allPoints$i[n+1] <<- lastPoint$i
+            state$savedNumber <- state$savedNumber + 1L
         } else if (key == "u") {
             n <- length(allPoints$key)
             if (n > 0L) {
                 allPoints$key <<- head(allPoints$key, -1L)
+                allPoints$filename <<- head(allPoints$filename, -1L)
                 allPoints$view <<- input$plotType
                 allPoints$x <<- head(allPoints$x, -1L)
                 allPoints$y <<- head(allPoints$y, -1L)
                 allPoints$i <<- head(allPoints$i, -1L)
-                msg("removed last point.")
+                state$savedNumber <- state$savedNumber - 1L
+                msg("removed last point; new length=", length(allPoints$i), " or ", state$savedNumber)
             }
         } else if (key == "s") {
-            print(file=stderr(), allPoints)
+            if (length(allPoints$x) < 1L) {
+                shiny::showNotification("No data to show yet. Press '?' to learn how to save data",
+                    type="error")
+            } else {
+                print(file=stderr(), as.data.frame(allPoints))
+            }
         } else if (key == "w") {
-            names(allPoints) <- c("key", "x", "y", "i", "view")
-            write.csv(allPoints, "oceInspect.csv", row.names=FALSE)
+            csv <- paste0(gsub("\\..*$", "", gsub(".*/","", data[["filename"]])), ".csv")
+            if (length(allPoints$x) < 1L) {
+                shiny::showNotification(paste0("No data to write to ", csv, "; Press '?' to learn how to save data"),
+                    type="error")
+            } else {
+                write.csv(as.data.frame(allPoints), csv, row.names=FALSE)
+            }
         } else if (key == "?") {
             shiny::showModal(shiny::modalDialog(title="Key-stroke commands",
                     shiny::HTML(keyPressHelp), easyClose=TRUE))
         }
     })
-
 }
 
 #' Inspect an oce CTD-like object
@@ -266,8 +265,15 @@ server <- function(input, output, session)
 #'
 #' @examples
 #' library(oceInspect)
+#' # Example 1: argo file
 #' if (interactive()) {
 #'     f <- system.file("extdata/R6903548_029.nc", package="oceInspect")
+#'     d <- oce::read.oce(f)
+#'     oceInspectApp(d)
+#' }
+#' # Example 2: ctd file
+#' if (interactive()) {
+#'     f <- system.file("extdata/ctd.cnv", package="oceInspect")
 #'     d <- oce::read.oce(f)
 #'     oceInspectApp(d)
 #' }
